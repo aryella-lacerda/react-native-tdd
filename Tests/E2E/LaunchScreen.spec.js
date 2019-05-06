9e815813726e43b41b2cc7b2ba94508601dabbdf
Aryella <aryella.lacerda@gmail.com>
2019-05-06 10:18:49 -0300

    E2E tests of restaurant screen

--- Tests/E2E/LaunchScreen.spec.js
+++ Tests/E2E/LaunchScreen.spec.js
@@ -5,7 +5,20 @@ describe('Managing Restaurants', () => {
     await device.reloadReactNative()
   })
 
-  it('should have welcome screen', async () => {
-    await expect(element(by.id('introText'))).toBeVisible()
+  const id = (_id) => element(by.id(_id))
+  const label = (_label) => element(by.label(_label))
+
+  it('should allow creating a restaurant', async () => {
+    const restaurant = 'Burger King'
+
+    // Sequence of events
+    await id('new-restaurant-button').tap()
+    await id('restaurant-name-text-field').typeText(restaurant)
+    await id('save-restaurant-button').tap()
+
+    // Check result
+    await expect(label(restaurant)).toBeVisible()
+    await expect(id('restaurant-name-text-field')).toBeNotVisible()
+
   })
 }) 
